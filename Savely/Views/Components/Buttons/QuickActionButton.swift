4fcac5593171769df5a092f392e46888f2a48e4b
Belli <ivanlorenzana@outlok.com>
2024-11-19 20:02:42 -0800

    UI: Correction on the QuickActionButton
    - Added cornerRadiusMedium UIConstant

--- Savely/Views/Components/Buttons/QuickActionButton.swift
+++ Savely/Views/Components/Buttons/QuickActionButton.swift
@@ -10,26 +10,32 @@ import SwiftUI
 struct QuickActionButton: View {
     var iconName: String
     var label: String
-    var backgroundColor: Color
+    let action: () -> Void
 
     var body: some View {
-        VStack {
-            Image(systemName: iconName)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 24, height: 24)
-                .foregroundColor(.white)
-            Text(label)
-                .font(.footnote)
-                .foregroundColor(.white)
-        }
-        .padding()
-        .background(backgroundColor)
-        .frame(width: .infinity, height: .infinity)
-        .cornerRadius(10)
+        Button(action: {
+            action()
+        }, label: {
+            VStack {
+                Image(systemName: iconName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.white)
+                Text(label)
+                    .font(.footnote)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .background(Color.green)
+            .frame(width: .infinity, height: .infinity)
+            .cornerRadius(UIConstants.UICornerRadius.cornerRadiusMedium)
+        })
     }
 }
 
 #Preview {
-    QuickActionButton(iconName: "camera", label: Strings.Buttons.addExpenseButton, backgroundColor: .green)
+    QuickActionButton(iconName: "camera", label: Strings.Buttons.addExpenseButton, action: {
+        print("Hola")
+    })
 }
